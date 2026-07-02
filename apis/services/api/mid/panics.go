b912b31d6b4f6c8cffbe5afbd7383b95901587b0
gsemer <gsemertzakis@hotmail.com>
2026-07-02 18:13:32 +0300

    panics middleware

---
+++ apis/services/api/mid/panics.go
@@ -0,0 +1,28 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gsemer/ardanlabs-service/app/api/mid"
+	"github.com/gsemer/ardanlabs-service/foundation/web"
+)
+
+// Panics executes the panic middleware functionality.
+func Panics() web.MidHandler {
+	m := func(handler web.Handler) web.Handler {
+
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+			hdl := func(ctx context.Context) error {
+				return handler(ctx, w, r)
+			}
+
+			return mid.Panics(ctx, hdl)
+		}
+
+		return h
+	}
+
+	return m
+}
